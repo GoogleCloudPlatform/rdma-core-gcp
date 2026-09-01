linux-rdma/rdma-core

ionic: map PHC state page for user space access
map the PHC page to allow applications to access hardware
timestamp information.

Signed-off-by: Allen Hubbe <allen.hubbe@amd.com>
Signed-off-by: Abhijit Gangurde <abhijit.gangurde@amd.com>

providers/ionic/ionic.h
@@ -79,6 +79,7 @@ struct ionic_ctx {
 
 	void			*dbpage_page;
 	uint64_t		*dbpage;
+	struct ionic_phc_state	*phc_state;
 
 	pthread_mutex_t		mut;
 	struct ionic_tbl_root	qp_tbl;

GoogleCloudPlatform/rdma-core-gcp

providers/ionic/ionic.h
@@ -103,6 +103,7 @@ struct ionic_ctx {
 
 	void			*dbpage_page;
 	uint64_t		*dbpage;
+	struct ib_uverbs_clock_info	*phc_state;
 
 	pthread_mutex_t		mut;
 	struct ionic_tbl_root	qp_tbl;