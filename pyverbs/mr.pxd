linux-rdma/rdma-core

pyverbs: Add ibv_buf provider-aware buffer support
Wrap the provider-aware buffer API (ibv_alloc_buf, ibv_free_buf,
ibv_reg_buf_mr) with new Buf and BufMR classes. Buf owns a buffer
allocated through a PD and is tracked in a per-PD weakset, so it is
torn down before the PD it belongs to. BufMR registers an MR over a
(sub)range of a Buf and deregisters it on close without freeing the
underlying buffer.

Signed-off-by: Jiri Pirko <jiri@nvidia.com>

pyverbs/mr.pxd
@@ -25,6 +25,17 @@ cdef class MR(PyverbsCM):
 cdef class MREx(MR):
     cdef object dmah
 
+cdef class Buf(PyverbsCM):
+    cdef v.ibv_buf *bufh
+    cdef void *addr
+    cdef size_t size
+    cdef object pd
+    cdef object mrs
+    cdef add_ref(self, obj)
+
+cdef class BufMR(MR):
+    cdef object cocobuf
+
 cdef class MWBindInfo(PyverbsCM):
     cdef v.ibv_mw_bind_info info
     cdef object mr

GoogleCloudPlatform/rdma-core-gcp

pyverbs/mr.pxd
@@ -24,6 +24,18 @@ cdef class MR(PyverbsCM):
 
 cdef class MREx(MR):
     cdef object dmah
+    cdef object backing_buf
+
+cdef class Buf(PyverbsCM):
+    cdef v.ibv_buf *bufh
+    cdef void *addr
+    cdef size_t size
+    cdef object pd
+    cdef object mrs
+    cdef add_ref(self, obj)
+
+cdef class BufMR(MR):
+    cdef object backing_buf
 
 cdef class MWBindInfo(PyverbsCM):
     cdef v.ibv_mw_bind_info info